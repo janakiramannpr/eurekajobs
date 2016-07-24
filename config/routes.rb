d25f0a5532459ee908257e0c607d94f6eafca6d8
janakiramannpr <akerueonline@gmail.com>
2016-07-24 18:22:03 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-   root   'static_pages#home'
+root   'static_pages#home'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
   post    '/signup',  to: 'users#create'
@@ -8,8 +7,3 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> cbb033bf10b9f921b43ce3b7cad729b40b5c65d3
-end
