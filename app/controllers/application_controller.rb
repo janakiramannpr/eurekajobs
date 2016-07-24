a6145829e011cdf149007b79ab22116e296424a3
janakiramannpr <akerueonline@gmail.com>
2016-07-24 18:25:08 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
+ 
   include SessionsHelper
-=======
->>>>>>> cbb033bf10b9f921b43ce3b7cad729b40b5c65d3
-end
+ 
+ end
